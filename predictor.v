347f6945e7211805c5dce814da757c8d51f7998d
smhh22 <94788432+smhh22@users.noreply.github.com>
2023-06-01 10:13:51 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -1,15 +1,18 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
 	reg [1:0] counter;
+	initial begin
+		counter = 2'h0;
+	end
 	always @(posedge clk) begin
 		if (request) begin
 			prediction <= counter[1];
 		end
 		if (result) begin
 			if (taken) begin
-				if (counter < 3) counter <= counter + 1;
+				if (counter < 2'h3) counter <= counter + 1;
 			end
 			else begin
-				if (counter > 0) counter <= counter - 1;
+				if (counter > 2'h0) counter <= counter - 1;
 			end
 		end
 	end
